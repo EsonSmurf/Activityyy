cef4573bcf493284099bfeb27e6c113c3abf3a58
Eson15 <151595276+EsonSmurf@users.noreply.github.com>
2023-12-12 23:19:16 +0800

    Add files via upload

--- RunQuad.java
+++ RunQuad.java
@@ -1,20 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package runquad;
 
-/**
- *
- * @author jovenchris
- */
-public class RunQuad {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Quadrilateral {
+    public void showDescription() {
+        System.out.println("- is a quadrilateral");
     }
-    
 }
+
+class Rectangle extends Quadrilateral {
+    @Override
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 right angles");
+    }
+}
+
+class Square extends Rectangle {
+    @Override
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 equal sides");
+    }
+}
+
+public class RunQuad {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press R for Rectangle or S for Square:");
+        char choice = scanner.next().charAt(0);
+
+        if (choice == 'R' || choice == 'r') {
+            Rectangle rectangle = new Rectangle();
+            rectangle.showDescription();
+        } else if (choice == 'S' || choice == 's') {
+            Square square = new Square();
+            square.showDescription();
+        } else {
+            System.out.println("Invalid choice. Please press R or S.");
+        }
+
+        scanner.close();
+    }
+}
+
